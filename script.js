a26267bb313027b6fd36e4dde9486745d28c845f
WalkOffDev <istochnik89@gmail.com>
2018-04-30 17:58:54 +0300

    added starting project

---
+++ script.js
@@ -0,0 +1,58 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function createButton() {
+	let button = document.createElement("button");
+	button.classList.add("buttonClass");
+	button.appendChild(document.createTextNode("Common! Take random gradient!!!"));
+	button.addEventListener("click",generateColors)
+	body.appendChild(button);
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function generateColors() {
+    color1.value = getRandomColor();
+	color2.value = getRandomColor();
+	// setGradient();
+}
+
+setGradient();
+createButton();
+// body.style.background = "linear-gradient(to right,${color1.value},${color2.value})";
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+
+
+// A few things you can try:
+
+// 1. Write code so that the colour inputs match the background generated on the first page load. 
+
+// 2. Display the initial CSS linear gradient property on page load.
+
+// 3. BONUS: Add a random button which generates two random numbers for the colour inputs.
+
+// Good luck!
